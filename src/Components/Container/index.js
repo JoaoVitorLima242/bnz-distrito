bc947acb105646bd611f9c2b283c2569e134dd86
JoaoVitorLima242 <jlima0500@gmail.com>
2022-07-18 19:00:04 -0300

    add header and footer to container

--- src/Components/Container/index.js
+++ src/Components/Container/index.js
@@ -3,8 +3,23 @@ import './styles.css'
 const Container = () => {
     return (
         <div className="container view">
-            <div>
+            <div className='card'>
+                <div className='header'>
+                    <h2>Ganhe R$400,00 em serviços jurídicos + call com advogado grátis!</h2>
+                    <h3>Especial para Startups parceiras da Distrito</h3>
+                </div>
+                <div className='content'>
+                    <div>
 
+                    </div>
+                    <div>
+
+                    </div>
+                </div>
+                <div className='footer'>
+                    <h3>Cadastre-se na nossa plataforma e simplifique seu jurídico!</h3>
+                    <button>Consulte nossos planos</button>
+                </div>
             </div>
         </div>
     )
