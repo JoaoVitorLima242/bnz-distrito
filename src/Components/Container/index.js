0a8ee0d4299a0b95c5dc1d360f3ef815661df2ec
JoaoVitorLima242 <jlima0500@gmail.com>
2022-07-19 10:05:05 -0300

    content

--- src/Components/Container/index.js
+++ src/Components/Container/index.js
@@ -1,4 +1,5 @@
 import './styles.css'
+import Img from '../../Assets/Images/img1.png'
 
 const Container = () => {
     return (
@@ -9,11 +10,18 @@ const Container = () => {
                     <h3>Especial para Startups parceiras da Distrito</h3>
                 </div>
                 <div className='content'>
-                    <div>
-
+                    <div className='img-content'>
+                        <img src={Img} alt="Mulher falando no celular enquanto uso o seu notebook"/>
                     </div>
                     <div>
-
+                        <p>A Bonuz é um escritório jurídico digital, capaz de oferecer agilidade e custos de 30% a 50% mais acessíveis que escritórios convencionais, através de uma plataforma simples e intuitiva.</p>
+                        <p>Você pode solicitar uma conversa com advogado para orientação trabalhista, tributária ou auxílio em outros assuntos, como:</p>
+                        <ul>
+                            <li>Revisão e elaboração de contrato de sócios e colaboradores</li>
+                            <li>Contrato de Vesting</li>
+                            <li>Registro de marca e patentes</li>
+                            <li>Política de uso para aplicativos e sites.</li>
+                        </ul>
                     </div>
                 </div>
                 <div className='footer'>
